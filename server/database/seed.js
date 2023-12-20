eb360dbb4738edc80fd31a0b09225540edf96fba
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-12-19 20:46:15 -0500

    added stock images

--- server/database/seed.js
+++ server/database/seed.js
@@ -19,7 +19,7 @@ db.once('open', async () => {
         {
             name: 'White T-shirt',
             description: 'Casual white t-shirt for a laid-back look.',
-            img: 'white-tshirt.jpg',
+            img: 'img-1.jpg',
             category: categories[0]._id,
             price: 29.99,
             quantity: 500,
@@ -27,7 +27,7 @@ db.once('open', async () => {
         {
             name: 'Graphic Print T-shirt',
             description: 'Make a statement with this bold graphic print t-shirt.',
-            img: 'graphic-tshirt.jpg',
+            img: 'img-2.jpg',
             category: categories[0]._id,
             price: 22.99,
             quantity: 50,
@@ -35,7 +35,7 @@ db.once('open', async () => {
         {
             name: 'Vintage T-shirt',
             description: 'Rock a vintage look with this t-shirt.',
-            img: 'vintage-tshirt.jpg',
+            img: 'img-3.jpg',
             category: categories[0]._id,
             price: 25.99,
             quantity: 30,
@@ -43,7 +43,7 @@ db.once('open', async () => {
         {
             name: 'Cozy Hoodie Sweatshirt',
             description: 'Stay warm and stylish with this cozy hoodie sweatshirt.',
-            img: 'hoodie-sweatshirt.jpg',
+            img: 'img-4.jpg',
             category: categories[1]._id,
             price: 39.99,
             quantity: 500,
@@ -51,7 +51,7 @@ db.once('open', async () => {
         {
             name: 'Classic Black Hoodie',
             description: 'A classic black hoodie that never goes out of style.',
-            img: 'black-hoodie.jpg',
+            img: 'img-5.jpg',
             category: categories[1]._id,
             price: 54.99,
             quantity: 100,
@@ -59,7 +59,7 @@ db.once('open', async () => {
         {
             name: 'Warm Fleece Hoodie',
             description: 'Stay warm in style with this fleece-lined hoodie.',
-            img: 'fleece-hoodie.jpg',
+            img: 'img-6.jpg',
             category: categories[1]._id,
             price: 49.99,
             quantity: 100,
@@ -67,7 +67,7 @@ db.once('open', async () => {
         {
             name: 'Floral Sundress',
             description: 'Elegant floral sundress perfect for any occasion.',
-            img: 'sundress.jpg',
+            img: 'img-7.jpg',
             category: categories[2]._id,
             price: 49.99,
             quantity: 500,
@@ -75,7 +75,7 @@ db.once('open', async () => {
         {
             name: 'Blue Summer Dress',
             description: 'Light and breezy blue summer dress for hot days.',
-            img: 'blue-summer-dress.jpg',
+            img: 'img-8.jpg',
             category: categories[2]._id,
             price: 44.99,
             quantity: 20,
@@ -83,7 +83,7 @@ db.once('open', async () => {
         {
             name: 'Elegant Red Dress',
             description: 'Turn heads with this elegant red dress.',
-            img: 'red-dress.jpg',
+            img: 'img-9.jpg',
             category: categories[2]._id,
             price: 59.99,
             quantity: 30,
